2414119b0eb9db1b9a15ab01c72b3145acfbe35a
Aldrick Bonaobra <git@aldrickb.com>
2021-02-08 16:00:09 +0800

    Update sidemenu

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -4,7 +4,7 @@ const config: MenuEntry[] = [
   {
     label: 'Home',
     icon: 'HomeIcon',
-    href: 'https://pancakeswap.finance/'
+    href: 'https://app.srk.finance/'
   },
   {
     label: 'Trade',
@@ -22,60 +22,9 @@ const config: MenuEntry[] = [
     ]
   },
   {
-    label: 'Farms',
-    icon: 'FarmIcon',
-    href: 'https://pancakeswap.finance/farms'
-  },
-  {
-    label: 'Pools',
-    icon: 'PoolIcon',
-    href: 'https://pancakeswap.finance/syrup'
-  },
-  {
-    label: 'Lottery',
-    icon: 'TicketIcon',
-    href: 'https://pancakeswap.finance/lottery'
-  },
-  {
-    label: 'NFT',
-    icon: 'NftIcon',
-    href: 'https://pancakeswap.finance/nft'
-  },
-  {
-    label: 'Info',
-    icon: 'InfoIcon',
-    items: [
-      {
-        label: 'Overview',
-        href: 'https://pancakeswap.info'
-      },
-      {
-        label: 'Tokens',
-        href: 'https://pancakeswap.info/tokens'
-      },
-      {
-        label: 'Pairs',
-        href: 'https://pancakeswap.info/pairs'
-      },
-      {
-        label: 'Accounts',
-        href: 'https://pancakeswap.info/accounts'
-      }
-    ]
-  },
-  {
-    label: 'IFO',
-    icon: 'IfoIcon',
-    href: 'https://pancakeswap.finance/ifo'
-  },
-  {
     label: 'More',
     icon: 'MoreIcon',
     items: [
-      {
-        label: 'Voting',
-        href: 'https://voting.pancakeswap.finance'
-      },
       {
         label: 'Github',
         href: 'https://github.com/pancakeswap'
