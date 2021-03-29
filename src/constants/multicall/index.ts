02b8fee5af8a3a19ef5b90487db5e578d282b613
Aldrick Bonaobra <git@aldrickb.com>
2021-03-29 15:41:24 +0800

    Update contract address for mainnet

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@sparkpointio/sparkswap-sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x097e3CbD07Edb4FB0dB72568443Dc5472C185011', // TODO
+  [ChainId.MAINNET]: '0x98e8EB714dBBBAaC631A6866BD57423d15d53043', // TODO
   [ChainId.BSCTESTNET]: '0x98e8EB714dBBBAaC631A6866BD57423d15d53043'
 }
 
--- src/constants/v1/index.ts
+++ src/constants/v1/index.ts
@@ -4,7 +4,7 @@ import V1_EXCHANGE_ABI from './v1_exchange.json'
 import V1_FACTORY_ABI from './v1_factory.json'
 
 const V1_FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0xC07d4604400139108BbdB3076636365A385879eF', // TODO
+  [ChainId.MAINNET]: '0x199DfE1D57e8E1f7A403CCfe6c5a0CC4C98b704F', // TODO
   [ChainId.BSCTESTNET]: '0x199DfE1D57e8E1f7A403CCfe6c5a0CC4C98b704F'
 }
 
