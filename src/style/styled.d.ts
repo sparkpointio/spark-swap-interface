d823539c20f13fc352fdb47b08afb8d3c0290e5a
Aldrick Bonaobra <git@aldrickb.com>
2021-04-01 17:44:51 +0800

    Fix DefaultTheme to SparkSwapTheme

--- src/style/styled.d.ts
+++ src/style/styled.d.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-unresolved
-import { PancakeTheme } from '@sparkpointio/sparkswap-uikit/dist/theme'
+import { SparkSwapTheme } from '@sparkpointio/sparkswap-uikit/dist/theme'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface DefaultTheme extends PancakeTheme {}
+  export interface DefaultTheme extends SparkSwapTheme {}
 }
