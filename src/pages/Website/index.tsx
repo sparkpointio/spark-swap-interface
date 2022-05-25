59a5d1306b31c64120c9d451b552341a6ed7b8ab
Justin <justinanthony.primary@gmail.com>
2022-05-25 14:17:37 +0800

    [chore] import style with new font

--- src/pages/Website/index.tsx
+++ src/pages/Website/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import * as AppSections from './components'
 import './assets/fonts/quatro/stylesheet.css'
+import './components/styles/_style.css'
 
 const Sections = (AppSections as unknown) as { [key: string]: React.FC};
 const App = () => {
