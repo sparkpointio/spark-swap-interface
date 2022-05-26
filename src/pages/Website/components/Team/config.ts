4f6eba2709af7d6defc80a727f1b0c78139edd73
Mat Ivan Arquero <mativanarquero@gmail.com>
2022-05-26 17:41:50 +0800

    Add component for team

---
+++ src/pages/Website/components/Team/config.ts
@@ -0,0 +1,31 @@
+
+const TeamData = [
+    {
+      name: 'Aldrick Banaobra',
+      position: 'LEAD BLOCKCHAIN DEVELOPER',
+      image: 'Aldrick'
+    },
+    {
+      name: 'Medard Mandane',
+      position: 'LEAD BACKEND DEVELOPER',
+      image: 'Medard'
+    },
+    {
+      name: 'Jan Balbin',
+      position: 'LEAD QA SPECIALIST',
+      image: 'Balbz'
+    },
+    {
+      name: 'Koji Adriano',
+      position: 'FULL STACK DEVELOPER',
+      image: 'Koji'
+    },
+    {
+      name: 'Mat Ivan Arquero',
+      position: 'FULL STACK DEVELOPER',
+      image: 'Mat'
+    },
+    
+  ];
+
+  export default TeamData;
