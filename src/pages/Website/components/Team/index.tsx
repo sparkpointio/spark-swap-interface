1b503576e87f692fdba290681a35ed72afe1fcb7
Mat Ivan Arquero <mativanarquero@gmail.com>
2022-05-30 22:11:29 +0800

    Use layout breakpoints for team section custom mobile styles

--- src/pages/Website/components/Team/index.tsx
+++ src/pages/Website/components/Team/index.tsx
@@ -5,6 +5,7 @@ import TeamData from './config';
 import { Title } from '../Elements'
 import { TeamType } from './types';
 import PageSection from '../styles/Layout'
+import { breakpoints } from '../styles/Layout/Breakpoints'
 
 const Wrapper = styled.div`
   flex-wrap: wrap;
@@ -15,7 +16,7 @@ const Wrapper = styled.div`
 
 const StyledSubHeading = styled(Text)`
  color: #39BEEC;
- @media (max-width: 500px) {
+ @media screen and (max-width: ${breakpoints.Mobile.l}px) {
   margin-bottom: 30px;
 }
 `
@@ -27,7 +28,7 @@ const StyledImage = styled.img`
   height: 300px;
   border-radius: 50%;
   padding: 10px;
-  @media (max-width: 500px) {
+  @media screen and (max-width: ${breakpoints.Mobile.l}px) {
     width: 250px;
     height: 250px;
     margin: auto;
