b7b99831b315c707f8208e660ff965f7c662d581
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-05-30 16:22:00 +0800

    Update newsletter signup

--- src/pages/Website/components/Newsletter/SignUp.tsx
+++ src/pages/Website/components/Newsletter/SignUp.tsx
@@ -23,33 +23,30 @@ const Container = styled.div`
 -webkit-box-shadow: 1px 6px 13px -3px rgba(57,190,236,0.75);
 -moz-box-shadow: 1px 6px 13px -3px rgba(57,190,236,0.75);
 `
-const InputContainer = styled(Flex)`
-  & > * {
-    margin: 10px 0px;
-  }
-`
+const InputContainer = styled(Flex)``
 
 const Background = styled.img`
   width: 260px;
   position: absolute;
   top: 0;
   margin: 30px 30px 0px 30px;
-  
 `
 
 const Message = styled(Text)`
   font-family: 'Quatro';
   z-index: 1; 
   position: relative;
+  @media screen and (max-width: ${breakpoints.Mobile.l}px) {
+    padding: 0px;
+    text-align: center;
+  }
 `
 
 const NewsletterWidget = styled.iframe<{screenSize?: number}>`
-  min-height: 33vh;
-  width: 600px;
-  padding: 5px;
+  min-height: 25vh;
+  // width: 600px;
+  // padding: 5px;
   @media screen and (max-width: ${breakpoints.Mobile.l}px) {
-    min-height: 50vh;
-    width: 80vw;
     padding: 0px;
   }
 `
@@ -58,22 +55,21 @@ const NewsletterWidget = styled.iframe<{screenSize?: number}>`
 const AppComponent: React.FC = () => {
   return (
     <Container>
-      {/* <Grid container justifyContent='space-around' alignItems='center'>
-        <Grid item xs={12} sm={5} md={5}>
+      <Grid container justifyContent='space-around' alignItems='center'>
+        <Grid item xs={12} sm={6} md={5}>
+          <Flex style={{minHeight: '250px'}} alignItems='center'>
           <Background alt='mail-icon' src='/images/Website/Mail.png'/>
           <Message fontSize='1.29em' as="p" color={Colors.text1}>
             Signing up to the newsletter means consenting to receiving marketing and news emails from SparkSwap
           </Message>
+          </Flex>
         </Grid>
-        <Grid item xs={12} sm={5} md={5}>
+        <Grid item xs={12} sm={6} md={5}>
           <InputContainer flexDirection='column'>
-          <Text fontSize='1.3em' as="p" color={Colors.accent3}>Email Address</Text>
-          <Input type="text" />
-          <SignUpButton>Subscribe Now</SignUpButton>
+          <NewsletterWidget id='newsletter' title='newsletter' src="https://app.mailjet.com/widget/iframe/5Eqa/NdK" width="100%" frameBorder="0" scrolling="no"/>
           </InputContainer>
         </Grid>
-      </Grid> */}
-        <NewsletterWidget id='newsletter' title='newsletter' src="https://app.mailjet.com/widget/iframe/5Eqa/GUa" width="100%" frameBorder="0" scrolling="no"/>
+      </Grid>
     </Container>
   )
 }
