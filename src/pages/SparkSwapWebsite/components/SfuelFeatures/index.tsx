8bbd66c84ec4b68a26d2ffe989109f54d36f04f8
Justin <justinanthony.primary@gmail.com>
2022-06-06 14:54:14 +0800

    [style/feat] new style for title, sfuel token sections and dynamic data for supply

---
+++ src/pages/SparkSwapWebsite/components/SfuelFeatures/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+import PageSection from '../styles/Layout'
+import { Title, Card } from '../Elements'
+import { Colors } from '../styles/Layout/Colors'
+
+const FlexContainerStretch = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: stretch;
+  gap: 2em;
+  width: 100%;
+`
+
+const SfuelFeatures = () => {
+  return (
+    <PageSection direction='column' padding='8em 0em 8em 0em' background={Colors.background2}>
+      <Title value='Features of the SFUEL Token' />
+      <FlexContainerStretch>
+        <Card background='#191B1F' src='images/Website/5 DAO.png' heading='SPARKSWAP DAO' body='Decentralized governance of the SparkSwap DeFi Platform.' />
+        <Card background='#191B1F' src='images/Website/5 Incentive.png' heading='GOVERNANCE INCENTIVES' body='SFUEL token holders who participate in SparkDeFi governance for its continued and sustained improvement and growth are entitled to SFUEL.' />
+        <Card background='#191B1F' src='images/Website/5 Rewards.png' heading='STAKING REWARDS' body='SFUEL tokens are used as rewards in our Liquidity Staking/Mining and Savings Pools when users stake either SRK or SFUEL pairs.' />
+      </FlexContainerStretch>
+    </PageSection>
+  )
+}
+
+export default SfuelFeatures
