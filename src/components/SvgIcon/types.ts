ae5de53d178a482e079bab79b713480dea21f003
Mat Ivan Arquero <mativanarquero@gmail.com>
2022-06-08 20:47:00 +0800

    Add svg components

---
+++ src/components/SvgIcon/types.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from 'styled-components'
+import { SVGAttributes } from 'react'
+import { SpaceProps } from 'styled-system'
+
+export interface SvgProps extends SVGAttributes<HTMLOrSVGElement>, SpaceProps {
+  theme?: DefaultTheme
+}
