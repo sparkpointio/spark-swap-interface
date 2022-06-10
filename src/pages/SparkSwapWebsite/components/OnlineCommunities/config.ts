6d6b22bd42aae1520739ae0a2573ac0b5e0d8027
Mat Ivan Arquero <mativanarquero@gmail.com>
2022-06-10 09:48:15 +0800

    Add online communities section and components

---
+++ src/pages/SparkSwapWebsite/components/OnlineCommunities/config.ts
@@ -0,0 +1,35 @@
+export interface Socials {
+    name: string;
+    href: string;
+  }
+  
+  export const socials: Socials [] = [
+    {
+      name: 'Twitter',
+      href: 'https://twitter.com/SparkDeFi'
+    },
+    {
+      name: 'Facebook',
+      href: 'https://www.facebook.com/SparkDeFi'
+    },
+    {
+      name: 'Instagram',
+      href: 'https://www.instagram.com/sparkdefi'
+    },
+    {
+      name: 'Discord',
+      href: 'https://discord.com/invite/Sgc6yDEAAe'
+    },
+    {
+      name: 'Youtube',
+      href: 'https://www.youtube.com/channel/sparkpointio'
+    },
+    {
+      name: 'Medium',
+      href: 'https://medium.com/theecosystem'
+    },
+    {
+      name: 'Reddit',
+      href: 'https://www.reddit.com/r/SparkPoint/'
+    },
+  ]
