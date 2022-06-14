323294c85f233dd2904bb5163ef0c1129368b672
Justin <justinanthony.primary@gmail.com>
2022-06-14 15:04:31 +0800

    [feat] graphql d liquidity and volume data

--- src/pages/SparkSwapWebsite/components/Hero/Hero.tsx
+++ src/pages/SparkSwapWebsite/components/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 // import { useLocation, Route, useRouteMatch } from 'react-router-dom'
 import styled, { ThemeContext } from 'styled-components'
 import { Text, Flex, Heading, Button } from '@sparkpointio/sparkswap-uikit'
@@ -8,6 +8,7 @@ import { TwoColumnHeader } from '../styles/Column'
 import PageSection from '../styles/Layout'
 import { StyledContainer, ImageContainer, ButtonContainer } from '../styles/Containers'
 import { breakpoints } from '../styles/Layout/Breakpoints'
+import { Colors } from '../styles/Layout/Colors'
 
 const DynamicImageContainer = styled(ImageContainer)`
   margin: auto;
@@ -121,13 +122,112 @@ const ImageDiv = styled(Flex)`
   }
 `
 
+const VolumeWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3em;
+`
+
+const VolumeContainer = styled.div``
+
+const VolumeTitle = styled.p`
+  color: ${Colors.accent3};
+`
+
+const VolumeValue = styled.p`
+  color: ${Colors.text1};
+  font-size: 2em;
+`
+
 const Website: React.FC = () => {
   const theme = useContext(ThemeContext)
+
+  const [totalLiquidityAndVolumeUSD, setTotalLiquidityAndVolumeUSD] = useState({
+    totalLiquidity: 0,
+    totalVolume: 0
+  })
+  const [totalVolumeUSD, setTotalVolumeUSD] = useState(0)
+
+  const date = new Date()
+  const timestampNow = Math.floor(Date.now() / 1000)
+  const timestamp24hAgo = Math.floor(date.setDate(date.getDate() - 1) / 1000)
+  const apiEndpoint = 'https://api.thegraph.com/subgraphs/name/sparkpointio/spark-swap'
+  const apiBlockEndpoint = 'https://api.thegraph.com/subgraphs/name/aldrickb/test2sparkblock'
+
+  const getGraphQLData = () => {
+    fetch(apiEndpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: `{
+          uniswapFactories(first: 1) {
+            totalLiquidityUSD,
+            totalVolumeUSD
+          }
+        }`
+      })
+    })
+      .then(res => res.json())
+      .then((data) => setTotalLiquidityAndVolumeUSD({
+        totalLiquidity: data?.data?.uniswapFactories[0]?.totalLiquidityUSD,
+        totalVolume: data?.data?.uniswapFactories[0]?.totalVolumeUSD
+      }))
+      .catch(console.error);
+  }
+
+  const getGraphQLDataBlock = (block) => {
+    fetch(apiEndpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: `{
+          uniswapFactories(first: 1 block: { number: ${block} }) {
+            totalVolumeUSD
+          }
+        }`
+      })
+    })
+      .then(res => res.json())
+      .then((data) => setTotalVolumeUSD(totalLiquidityAndVolumeUSD.totalVolume - data?.data?.uniswapFactories[0]?.totalVolumeUSD))
+      .catch((err) => console.error(err));
+  }
+
+  const getGraphQLBlocks = () => {
+    fetch(apiBlockEndpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: `{
+          blocks(first: 1 orderBy: timestamp orderDirection: asc where: { timestamp_gt: ${timestamp24hAgo} timestamp_lt: ${timestampNow}}) {
+            id
+            number
+            timestamp
+          }
+        }`
+      })
+    })
+      .then(res => res.json())
+      .then((data) => {
+        const computedVolume = data?.data?.blocks[0]?.number
+        getGraphQLDataBlock(computedVolume)
+      })
+      .catch(console.error);
+  }
+
+  useEffect(() => {
+    getGraphQLData()
+    getGraphQLBlocks()
+  })
 
   return (
     <PageSection direction="column" id="hero">
       <BG>
         <HomeWrapper>
+          <ImageDiv>
+            <DynamicImageContainer>
+              <HeroSwap/>
+            </DynamicImageContainer>
+          </ImageDiv>
           <StyledFlex flexDirection="column">
             <HeadingContainer>
               <img src="images/Website/sparkswap-heading.png" alt="SparkSwap Heading" />
@@ -141,12 +241,17 @@ const Website: React.FC = () => {
                 Launch App
               </Button>
             </ButtonContainer>
+            <VolumeWrapper>
+              <VolumeContainer>
+                <VolumeTitle>Total Liquidity</VolumeTitle>
+                <VolumeValue>{totalLiquidityAndVolumeUSD.totalLiquidity !== 0 ? `$${totalLiquidityAndVolumeUSD.totalLiquidity.toString().split('.')[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",")}` : '$0.00'}</VolumeValue>
+              </VolumeContainer>
+              <VolumeContainer>
+                <VolumeTitle>Total Volume</VolumeTitle>
+                <VolumeValue>{totalVolumeUSD !== 0 ? `$${totalVolumeUSD.toString().split('.')[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",")}` : '$0.00'}</VolumeValue>
+              </VolumeContainer>
+            </VolumeWrapper>
           </StyledFlex>
-          <ImageDiv>
-            <DynamicImageContainer>
-              <HeroSwap/>
-            </DynamicImageContainer>
-          </ImageDiv>
         </HomeWrapper>
       </BG>
     </PageSection>
