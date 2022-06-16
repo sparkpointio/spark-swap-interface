e00e0dad7039feec06ee8a31eaae80e00aa11ae7
kojiadrianojr <iisparkplugiiog@gmail.com>
2022-06-16 14:38:48 +0800

    Update roadmap details

--- src/pages/SparkSwapWebsite/config/constants/Roadmap.ts
+++ src/pages/SparkSwapWebsite/config/constants/Roadmap.ts
@@ -9,33 +9,30 @@ export const Roadmaps: RoadmapConfig = {
         },
       ],
       February: [{ value: 'SparkNFT Platform: New features' }],
-      March: [{ value: 'SFUEL Quarterly Community Airdrop' }, { value: 'New Trading Pairs in SparkSwap' }],
+      March: [{ value: 'New Trading Pairs in SparkSwap' }],
     },
     Q2: {
       June: [
         {
           value: 'SparkSwap V2 ( June to July )',
-        },
-        {
-          value: 'New SparkSwap Homepage',
-        },
-        {
-          value: 'Launch of DEX V2',
-        },
-        {
-          value: 'Launch of Info Page V2',
-        },
-        {
-          value: 'Launch of API V2',
-        },
-        {
-          value: 'Launch of "Reward Token"',
+          items: [{
+            value: 'New SparkSwap Homepage',
+          },
+          {
+            value: 'Launch of DEX V2',
+          },
+          {
+            value: 'Launch of Info Page V2',
+          },
+          {
+            value: 'Launch of API V2',
+          },
+          {
+            value: 'Launch of SparkSwap Reward Token',
+          }]
         },
         {
           value: 'SFUEL Quarterly Community Airdrop',
-        },
-        {
-          value: 'Quarterly BuyBack and Burn',
         },
         {
           value: 'New trading pairs in SparkSwap',
@@ -45,24 +42,43 @@ export const Roadmaps: RoadmapConfig = {
     Q3: {
       July: [
         {
-          value: 'SparkPool: Revamp/Refresh',
+          value: 'SparkPool: New Feature',
         },
       ],
       August: [
         {
-          value: 'SparkSwap: Launch of SparkSwap VIP Club',
+          value: 'SparkSwap: SparkSwap VIP Club Integration',
         },
+        {
+          value: 'SparkSwap DEX: New Feature'
+        }
       ],
       September: [
         {
-          value: 'Quarterly Community Airdrop',
+          value: 'SparkRewards: New Feature',
         },
         {
-          value: 'Quarterly BuyBack and Burn',
+          value: 'SparkSwap Reward Token Community Airdrop',
+        },
+        {
+          value: 'SparkSwap Reward Token Buy Back and Burn'
+        },
+        {
+          value: 'New Trading pars in SparkSwap V2'
         },
       ],
     },
     Q4: {
+      October: [
+        { 
+          value: 'SparkPool: New Feature'
+        },
+      ],
+      November: [ 
+        {
+          value: 'SparkLaunch: New Feature'
+        }
+      ],
       December: [
         {
           value: 'SparkBridge: Multi-chain support',
