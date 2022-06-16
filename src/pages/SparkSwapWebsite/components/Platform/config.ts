85e63588078082b1040298463efd201fc28affc6
Mativanarquero <mativanarquero@gmail.com>
2022-06-16 12:01:43 +0800

    Add learn more to other apps and supply link

--- src/pages/SparkSwapWebsite/components/Platform/config.ts
+++ src/pages/SparkSwapWebsite/components/Platform/config.ts
@@ -3,43 +3,51 @@ const PlatformList = [
     image: 'DexIcon',
     title: 'SparkSwap DEX',
     description: 'Instantly swap multiple cryptocurrencies in a trustless and decentralized manner. Earn from the transaction fees when the user provides liquidity. ',
+    link: 'https://sparkswap.finance/#/swap',
   },
   {
     image: 'AnalyticsIcon',
     title: 'SparkSwap Analytics and Info',
     description:
       "Check the stats of all assets, pairs and liquidity pools available in the SparkSwap DEX. ",
+      link: 'https://sparkswap.info/#/home',
   },
   {
     image: 'PoolsIcon',
     title: 'SparkPool | Pools',
     description:
       "Pool-based staking is the simplest way to earn free tokens on SparkSwap. You stake and you earn token rewards, simple as that. ",
+    link: 'https://app.sparkswap.finance/#/pools',
   },
   {
     image: 'FarmsIcon',
     title: 'SparkPool | Farms',
     description: "Staking that rewards liquidity providers with SRK or SFUEL tokens. Add liquidity to a pair, earn Spark-LP tokens, stake and earn token rewards.",
+    link: 'https://app.sparkswap.finance/#/farms',
   },
   {
     image: 'BridgeIcon',
     title: 'SparkBridge',
     description: 'Cross-blockchain bridging for transferring Ethereum ERC-20 SRK to Binance Smart Chain as BEP-20 SRKb tokens and vice versa. SparkBridge will also support other token on future updates.',
+    link: 'https://app.sparkswap.finance/#/bridge',
   },
   {
     image: 'LaunchIcon',
     title: 'SparkLaunch IDO Launchpad Platform',
     description: 'Discover the hottest and latest projects launching in the blockchain world. Participate now and be onboarded and go to the moon! ',
+    link: 'https://app.sparkswap.finance/#/launch',
   },
   {
     image: 'NftIcon',
     title: 'SparkNFT Platform',
     description: 'SparkNFT enables enthusiasts, collectors, artists, fans, or anyone to buy, sell, and even manage their NFTs on the BSC network in a robust platform.',
+    link: 'https://nft.sparkswap.finance/#/home',
   },
   {
     image: 'RewardsIcon',
     title: 'SparkRewards',
     description: 'Sparkies’ place for all-things-rewards, aimed to reward the #SparkyCommunity through bounties, airdrops, and other exciting activities! The upcoming airdrops will soon launch on the SparkRewards platform. Just connect your wallet, and you are good to go. ',
+    link: 'https://app.sparkswap.finance/#/airdrop',
   },
   {
     image: 'DaoIcon',
